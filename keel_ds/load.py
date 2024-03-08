207a4a855af34e888954cab55c768fbe54cdb25e
Maicondalllg <maicon.dallagnol@unesp.br>
2024-03-08 16:38:58 -0300

    Upload data processed

--- keel_ds/load.py
+++ keel_ds/load.py
@@ -3,20 +3,27 @@ import numpy as np
 import pickle
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def list_data():
-    return os.listdir("../data/")
+    path1 = os.path.join(BASE_DIR, "data/imbalanced/raw")
+    path2 = os.path.join(BASE_DIR, "data/balanced/raw")
+    aux = os.listdir(path2)
+    aux.append(os.listdir(path2))
+
+    return aux
 
 
-def load_data(data, imbalanced=True):
+def load_data(data, imbalanced=False):
     if imbalanced:
         try:
-            return pickle.load(open(f"../data/imbalanced/processed/{data}.pkl", "rb"))
+            return pickle.load(open(os.path.join(BASE_DIR, f"data/imbalanced/processed/{data}.pkl"), "rb"))
         except:
             raise FileNotFoundError(f"File {data}.pkl not found")
 
     else:
         try:
-            return pickle.load(open(f"../data/balanced/processed/{data}.pkl", "rb"))
+            return pickle.load(open(os.path.join(BASE_DIR, f"data/balanced/processed/{data}.pkl"), "rb"))
         except:
             raise FileNotFoundError(f"File {data}.pkl not found")
